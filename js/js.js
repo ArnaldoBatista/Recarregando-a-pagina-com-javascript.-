26953a8b7c73cc7f5000eb31330eb64a79be8185
Arnaldo Batista <arnaldo.1565@hotmail.com>
2019-12-11 15:32:45 -0300

    melhorias no dinamismo da página

--- js/js.js
+++ js/js.js
@@ -4,15 +4,18 @@ let hora = data.getHours();
 let horario = new Date();
 //minutos local
 let min_atual = horario.getMinutes();
-let falt_h;
-let h_min;
-let h_mil;
-let min_hora_min;
+let falt_h, h_min, h_mil, min_hora_min;
 
-//verifico se o horario atual esta nos intervalos que quero calcular.
+var hora_id = document.getElementById('hora');
+
+//se o intervalo de horários for verdadeiro...
 if(hora >= 0 && hora < 12){
 //envio a msg para pagina de boas vindas.
 $("#boas-vindas").html(" Bom Dia!");
+//adiciono classe no qual irá carregar a cor de fundo.
+$("#cor-grad").addClass("c-m");
+//inserindo texto avisando quando ocorrerá novo onload
+hora_id.appendChild(document.createTextNode('12h'));
 //subtraiu o horario que quero recarregar a pagina - a hora atual.
 falt_h = (12 - hora);
 //converto o horario calculado anteriormente em minutos
@@ -22,17 +25,21 @@ min_hora_min = (hora_min - min_atual);
 //passo para a variavel os minutos convertidos em milissegundos para setar na função setTimeout.
 h_mil = (min_hora_min * 60000);
 }
-//mesmo exemplo da condição anterior
+
 if(hora >= 12 && hora < 18){
 $("#boas-vindas").html(" Boa Tarde!");
+$("#cor-grad").addClass("c-t");
+hora_id.appendChild(document.createTextNode('18h'));
 falt_h = (18 - hora);
 hora_min = (falt_h * 60);
 min_hora_min = (hora_min - min_atual);
 h_mil = (min_hora_min * 60000);
 }
-//mesmo exemplo da condição anterior
+
 if(hora >= 18 && hora < 24){
 $("#boas-vindas").html(" Boa Noite!");
+$("#cor-grad").addClass("c-n");
+hora_id.appendChild(document.createTextNode('0h'));
 falt_h = (24 - hora);
 hora_min = (falt_h * 60);
 min_hora_min = (hora_min - min_atual);
